0952342e96c3bf92646ff6a6deca7e26df5da7e6
ceimiplace <azamfiredenis@yahoo.com>
2022-09-08 17:45:15 +0300

    component gets user geolocation
    gets user geolocation and can render in case of fail or no permission

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
   }
   render() {
-    return <div>Hello world</div>;
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div> Latitude: {this.state.lat}</div>;
+    }
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 export default App;
