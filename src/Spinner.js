1fffde4c609527ddae4186a2945cf9d5fe2e336d
ceimiplace <azamfiredenis@yahoo.com>
2022-09-10 09:55:41 +0300

    refractored using tailwind

--- src/Spinner.js
+++ src/Spinner.js
@@ -6,9 +6,11 @@ class Spinner extends React.Component {
   }
   render() {
     return (
-      <div className={`spinner-container ${this.props.className}`}>
-        <div className="box"></div>
-        <div className="text">{this.props.text}</div>
+      <div
+        className={`spinner-container w-full h-full flex flex-col justify-center items-center bg-slate-700 text-white gap-10 ${this.props.className} `}
+      >
+        <div className="box h-24 w-24 border-white border-8 border-t-emerald-400 rounded-full animate-spin"></div>
+        <div className="text font-bold">{this.props.text}</div>
       </div>
     );
   }
